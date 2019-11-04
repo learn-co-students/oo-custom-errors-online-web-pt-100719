1deb2b2b7eceed94da773564310ea61700bf62ea
PBillingsby <philbillingsby@gmail.com>
2019-11-04 17:33:01 -0500

    Done.

---
+++ .history/lib/custom_errors_20191104113809.rb
@@ -0,0 +1,22 @@
+class Person
+  attr_accessor :partner, :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def get_married(person)
+    self.partner = person
+    person.partner = self
+  end
+  class PartnerError < StandardError
+  end
+end
+
+beyonce = Person.new("Beyonce")
+beyonce.get_married("Jay-Z")
+puts beyonce.name
+
+
+
+
